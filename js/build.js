399b8c2e83a2f0fbc5d9c9de05433e382587dd87
jmjuanes <josemijuanes@gmail.com>
2017-01-04 20:16:08 +0100

    js/build.js: build the module

---
+++ js/build.js
@@ -0,0 +1,22 @@
+//Build the Karyotype
+jviz.modules.karyoviewer.prototype.build = function()
+{
+  //Append the module
+  jviz.dom.append(this._parent, { id: this._id, class: this._class });
+
+  //Check the panel
+  if(this._panel.visible === true)
+  {
+    //Add the panel
+    this._panel.el = new jviz.components.panel(this._panel);
+
+    //Update the canvas panel
+    this._canvas.parent = this._panel.el.body();
+  }
+
+  //Build the canvas layer
+  this._canvas.el = new jviz.components.canvas(this._canvas);
+
+  //Return this
+  return this;
+};
