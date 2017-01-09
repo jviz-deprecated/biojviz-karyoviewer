639432aed01286a8e1f365d625504d7903f2fcf7
jmjuanes <josemijuanes@gmail.com>
2017-01-09 17:32:02 +0100

    js/resize.js: updated resize event

--- js/resize.js
+++ js/resize.js
@@ -2,13 +2,19 @@
 jviz.modules.karyoviewer.prototype.resize = function()
 {
   //Set the canvas width
-  //this._canvas.el.width(this._canvas.width);
+  //this._canvas.el.width(this._width);
 
   //Set the canvas height
-  this._canvas.el.height(this._canvas.height);
+  this._canvas.el.height(this._height);
 
   //Resize the canvas
   this._canvas.el.resize();
+
+  //Save the full width
+  this._width = this._canvas.el.width();
+
+  //Save the full height
+  //this._height = this._canvas.el.height();
 
   //Set to resize the chromosomes data
   this._chromosome.resized = false;
