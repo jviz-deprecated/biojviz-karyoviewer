c4179bbfb186965c627d3aa1d8720df5fe45b785
jmjuanes <josemijuanes@gmail.com>
2017-01-09 17:21:46 +0100

    js/orientation.js: parse the orientation

---
+++ js/orientation.js
@@ -0,0 +1,18 @@
+//Manage the orientation
+jviz.modules.karyoviewer.prototype.orientation = function(value)
+{
+  //Check the orientation value
+  if(typeof value !== 'string'){ return this._orientation; }
+
+  //convert to lower case
+  value = value.toLowerCase();
+
+  //Parse the orientation
+  if(['portrait', 'landscape'].indexOf(value) === -1){ value = 'portrait'; }
+
+  //Save the orientation
+  this._orientation = value;
+
+  //Continue
+  return this;
+};
