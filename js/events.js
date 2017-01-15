62cea4dfad5c0285a8db8456d0c392fab326ebfe
jmjuanes <josemijuanes@gmail.com>
2017-01-15 20:22:12 +0100

    js/events.js: draw label on move event

--- js/events.js
+++ js/events.js
@@ -42,71 +42,8 @@ jviz.modules.karyoviewer.prototype.eventDown = function(x, y)
 //Move event
 jviz.modules.karyoviewer.prototype.eventMove = function(x, y)
 {
-  //Get the canvas layer
-  var canvas = this._canvas.el.layer(this._features.name.layer);
-
-  //Get the features by position
-  var features = this.featuresByPosition(x, y, this._features.name.margin);
-
-  //Check the features list
-  if(features.length === 0)
-  {
-    //Remove the actual feature
-    this._features.name.actual = '';
-
-    //Clear the canvas and exit
-    return canvas.Clear();
-  }
-
-  //CHANGE WHEN WE HAVE THE ZOOM FUNCTION
-  //***********************************
-  //Get the first feature
-  var feature = features[0];
-
-  //Check the feature name
-  if(this._features.name.actual === feature.name){ return; }
-
-  //Clear the canvas
-  canvas.Clear();
-
-  //Update the actual feature
-  this._features.name.actual = feature.name;
-
-  //Initialize the tooltip coordinates
-  var coordinates = { posx: 0, posy: 0 };
-
-  //Check the orientation
-  if(this.isLandscape() === true)
-  {
-    //Set the x coordinate for landscape orientation
-    coordinates.posx = feature.posx + feature.width / 2;
-
-    //Set the y coordinate for landscape orientation
-    coordinates.posy = feature.posy + feature.height;
-  }
-  else
-  {
-    //Set the x coordinate for portrait orientation
-    coordinates.posx = feature.posx + feature.width;
-
-    //Set the y coordinate for protrait orientation
-    coordinates.posy = feature.posy + feature.height / 2;
-  }
-
-  //Move the tooltip
-  this._features.name.tooltip.move(coordinates);
-
-  //Set the tooltip text
-  this._features.name.tooltip.text(feature.name, false);
-
-  //Set the tooltip color
-  this._features.name.tooltip.color(feature.color);
-
-  //Draw the tooltip
-  this._features.name.tooltip.draw(canvas);
-
-  //Exit
-  return;
+  //Draw the label
+  this.labelDraw();
 };
 
 //Up event
