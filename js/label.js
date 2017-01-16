c4f9b1b5b1164a1df94bed7929e4188cf0f3dbec
jmjuanes <josemijuanes@gmail.com>
2017-01-16 11:11:29 +0100

    js/label.js: big rewrite. Call to overLabel to check if cursor is over the label

--- js/label.js
+++ js/label.js
@@ -4,34 +4,17 @@ jviz.modules.karyoviewer.prototype.labelDraw = function(x, y)
   //Check if label is visible
   if(this._label.visible === false){ return; }
 
-  //Get the canvas layer
-  var canvas = this._canvas.el.layer(this._label.layer);
+  //Check if is over a label
+  if(this.overLabel(x, y) === true){ return; }
 
-  //Check if label is active
-  if(this._label.active === true)
-  {
-    //Check the position for landscape
-    if(this.isLandscape() === true)
-    {
-      //Check the x position
-      if(Math.abs(x - this._label.position) <= this._label.offset){ return; }
-    }
-    //Check the position for portrait
-    else
-    {
-      //Check the y position
-      if(Math.abs(y - this._label.position) <= this._label.offset){ return; }
-    }
+  //Check if label is active and clear the canvas
+  if(this._label.active === true){ this.labelClear(); }
 
-    //Clear the canvas
-    canvas.Clear();
-  }
+  //Check if is over a chromosome
+  if(this._over.chromosome.actual === false){ return; }
 
-  //Set label as inactive
-  this._label.active = false;
-
-  //Get the features by position
-  this._label.features = this.featuresByPosition(x, y, this._label.offset);
+  //Get the features
+  this._label.features = this.overFeatures(this._over.chromosome.actual, x, y, this._label.offset);
 
   //Check the features list
   if(this._label.features.length === 0){ return; }
@@ -79,11 +62,74 @@ jviz.modules.karyoviewer.prototype.labelDraw = function(x, y)
   this._label.tooltip.el.color(feature.color);
 
   //Draw the tooltip
-  this._label.tooltip.el.draw(canvas);
+  this._label.tooltip.el.draw(this._canvas.el.layer(this._label.layer));
 
   //Set label active as true
   this._label.active = true;
 
+  //Save the label chromosome
+  this._label.chromosome = this._over.chromosome.actual;
+
+  //Check the orientation
+  if(this.isLandscape() === true)
+  {
+    //Add the label over width
+    this._label.over.width = this._label.tooltip.el._rectangle.width;
+
+    //Add the label over height
+    this._label.over.height = this._label.tooltip.el._rectangle.height + this._label.tooltip.el._triangle.height;
+
+    //Add the label over position x
+    this._label.over.posx = this._label.tooltip.el._rectangle.posx;
+
+    //Add the label over position y
+    this._label.over.posy = this._label.tooltip.el._rectangle.posy - this._label.tooltip.el._triangle.height;
+  }
+  else
+  {
+    //Add the label over width
+    this._label.over.width = this._label.tooltip.el._rectangle.width + this._label.tooltip.el._triangle.height;
+
+    //Add the label over height
+    this._label.over.height = this._label.tooltip.el._rectangle.height;
+
+    //Add the label over position x
+    this._label.over.posx = this._label.tooltip.el._rectangle.posx - this._label.tooltip.el._triangle.height;
+
+    //Add the label over position y
+    this._label.over.posy = this._label.tooltip.el._rectangle.posy;
+  }
+
   //Exit
   return;
+};
+
+//Label click event
+jviz.modules.karyoviewer.prototype.labelClick = function(x, y)
+{
+  //Check the number of features
+  if(this._label.features.length === 1)
+  {
+    //Get the feature
+    var feature = this._label.features[0];
+
+    //Emit the event and exit
+    return this._events.emit('click:feature', feature.name, feature.start, feature.end, feature.index);
+  }
+
+  //Exit
+  return;
+};
+
+//Clear the label
+jviz.modules.karyoviewer.prototype.labelClear = function()
+{
+  //Clear the canvas layer
+  this._canvas.el.layer(this._label.layer).Clear();
+
+  //Set label as inactive
+  this._label.active = false;
+
+  //Continue
+  return this;
 };
